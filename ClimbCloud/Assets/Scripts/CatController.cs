d1d0da46eb5e7b9d91ca80a83db3a2a2b6d620ed
DESKTOP-5OBM98S\SEOULIT <rlawo0307@naver.com>
2024-08-22 14:15:36 +0900

    고양이 애니메이션 추가

--- ClimbCloud/Assets/Scripts/CatController.cs
+++ ClimbCloud/Assets/Scripts/CatController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CatController : MonoBehaviour
 {
     public Rigidbody2D rb2D;
+    public SpriteRenderer sr;
+    public Animator animator;
     public float moveForce;
     public float jumpForce;
 
@@ -15,20 +17,30 @@ public class CatController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if(Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("왼쪽키 누름");
             this.rb2D.AddForce(new Vector2(-1 * moveForce, 0));
+            this.transform.localScale = new Vector3(-1, 1, 1); // GameObject의 Scale을 바꾸는 거라서 하위 object까지 모두 좌우 반전 됨
+            //sr.flipX = true;
+            this.animator.SetInteger("state", 1);
         }
-        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        else if(Input.GetKey(KeyCode.RightArrow))
         {
             Debug.Log("오른쪽키 누름");
             this.rb2D.AddForce(new Vector2(1 * moveForce, 0));
+            this.transform.localScale = new Vector3(1, 1, 1);
+            //sr.flipX = false;
+            this.animator.SetInteger("state", 1);
         }
-        else if(Input.GetKeyDown(KeyCode.Space))
+        else if(Input.GetKey(KeyCode.Space))
         {
             Debug.Log("스페이스 키 누름");
             this.rb2D.AddForce(new Vector2(0, 1 * jumpForce));
+        }
+        else
+        {
+            this.animator.SetInteger("state", 0);
         }
     }
 }
